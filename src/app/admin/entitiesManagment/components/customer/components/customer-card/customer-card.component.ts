272f16223b308c4970a5282c40488ce8a4dad270
michaldror26 <michaldror26@gmail.com>
2019-02-01 10:36:15 +0200

    complete with register & login

---
+++ src/app/admin/entitiesManagment/components/customer/components/customer-card/customer-card.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Customer} from '../../../../../../shared/models/Customer.class';
+import {ActivatedRoute} from '@angular/router';
+import {CustomerService} from '../../../../../../shared/services/customer.service';
+
+@Component({
+  selector: 'app-customer-card',
+  templateUrl: './customer-card.component.html',
+  styleUrls: ['./customer-card.component.css']
+})
+export class CustomerCardComponent implements OnInit {
+  @Input()
+  customer: Customer = null;
+
+  constructor(private _activateRout: ActivatedRoute,
+              private _customerService: CustomerService) {
+  }
+
+  ngOnInit() {
+    const id = this._activateRout.snapshot.params['id'];
+    this._customerService.getCustomer(id).subscribe((cust: Customer) => this.customer = cust);
+  }
+
+}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {Customer} from '../../shared/models/Customer.class';
+import {SiteUser} from '../../shared/models/SiteUser.class';
+import {CustomerService} from '../../shared/services/customer.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SiteUserService} from '../../shared/services/siteUser.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  customer: Customer;
+  siteUser: SiteUser;
+  message = '';
+  loginPage = false;
+
+  constructor(public _customerService: CustomerService,
+              private _activateRout: ActivatedRoute,
+              private _siteUserService: SiteUserService,
+              private _router: Router
+  ) {
+  }
+
+  async ngOnInit() {
+    this.customer = new Customer();
+    this.customer.SiteUser = new SiteUser();
+  }
+
+// בינתיים הרגיסטר הוא רק ללקוח
+  register() {
+    this._customerService.addCustomer(this.customer).subscribe(insertededCust => {
+        if (insertededCust) {
+          this.customer = insertededCust;
+          this.message = 'נרשמת בהצלחה!!';
+
+          setTimeout(function (router) {
+            router.navigate(['/login']);
+          }, 3000, this._router);
+
+        } else {
+          this.message = 'בדוק תקינות הפרטים שהזנת ונסה שוב';
+        }
+      },
+      error1 => {
+        this.message = error1.error.ExceptionMessage;
+      }
+    );
+  }
+}
